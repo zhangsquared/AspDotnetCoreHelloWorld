1f1542345135110cf455bcd9555936b10a893795
zhangsquared <hi.zhangzhang@gmail.com>
2020-07-30 04:52:21 +0100

    init commit

---
+++ AspDotnetCoreHelloWorld/Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace AspDotnetCoreHelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up hello world");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application hello world failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog((hostingContext, loggerConfiguration) =>
+            {
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                //webBuilder.UseStartup<Startup>();
+                ConfigKestrel(webBuilder);
+            });
+
+
+        private static void ConfigKestrel(IWebHostBuilder webBuilder)
+        {
+            webBuilder.UseKestrel(options => 
+            {
+                options.Listen(IPAddress.Loopback, 12344); // listen at port 12344
+            })
+            .Configure(app => 
+            {
+                app.Run(async context => await context.Response.WriteAsync("hello from zsquared"));
+            })
+            .UseIIS()
+            .UseIISIntegration();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+### Hello world for ASP .NET Core web 
+.NET 3.1
+
+## Related source code
+
+* [.NET Core](https://github.com/dotnet/core) - a cross-platform, open-source .NET platform
+* [ASP.NET Core](https://github.com/dotnet/aspnetcore) - a .NET Core framework for building web apps
+* [Extensions](https://github.com/dotnet/extensions) - Logging, configuration, dependency injection, and more.
+* [Documentation](https://github.com/aspnet/Docs) - documentation sources for https://docs.microsoft.com/aspnet/core/
+* [Entity Framework Core](https://github.com/dotnet/efcore) - data access technology
